8a6a2f71d4d5a32ec447fe39564e1701a90b82ad
Sarah-kasande <s.kasande@alustudent.com>
2024-10-23 18:22:17 +0200

    Update 1-top_ten.py
    my APIs

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python3
-"""Prints the title of the first 10 hot posts listed for a given subreddit"""
-
+"""
+This module contains the function top_ten.
+"""
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """Main function"""
-    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """
+    Returns the top ten posts for a given subreddit.
+    """
+    user = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
 
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        HOT_POSTS = RESPONSE.json().get("data").get("children")
-        [print(post.get('data').get('title')) for post in HOT_POSTS]
-    except Exception:
-        print(None)
+        response = requests.get(url, headers=user, allow_redirects=False)
+        response.raise_for_status()
+        data = response.json()
+
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children']:
+                print(post['data']['title'])
+            print("OK")  # Print OK only after successfully printing titles
+        else:
+            print("None")  # Handle case where subreddit exists but has no posts
+    except requests.exceptions.HTTPError:
+        print("None")  # Print None for HTTP errors (invalid subreddit)
+    except ValueError:
+        print("None")  # Print None for JSON decoding errors
+
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        top_ten(argv[1])
+    else:
+        print("Usage: ./script.py <subreddit>")
